3029f89501ed478dda849254c16196f0ba0c5c4d
Leroy Ware <leroyware@Leroys-MBP.home>
2021-10-03 01:36:49 -0500

    Minting App & Smart Contract Adjustments

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,23 @@
+import { UseWalletProvider } from 'use-wallet'
+import '../styles/styles.scss'
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+
+function App({ Component, pageProps }) {
+  	const isBrowser = typeof window !== "undefined"
+  	let chainId = 1
+	if (isBrowser) {
+	  chainId = window.location.hostname.includes('test') || window.location.hostname.includes('localhost') ? 4 : 1
+	}
+  return (
+  	<UseWalletProvider
+        chainId={4}
+        connectors={{
+          portis: { dAppId: 'samot-nft' },
+        }}
+    >
+  		<Component {...pageProps} />
+  	</UseWalletProvider>
+  )
+}
+
+export default App
