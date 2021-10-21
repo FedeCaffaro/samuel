bf11bcaf327346e72f5d22280b6044a2d8d869f7
Federico Caffaro <fcaffaro@itba.edu.ar>
2021-10-21 20:20:56 -0300

    final-updates

--- solidity/contracts/WhoIsSamot.sol
+++ solidity/contracts/WhoIsSamot.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "./ERC721Tradable.sol";
 import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
-
 // ██╗    ██╗██╗  ██╗ ██████╗     ██╗███████╗    ▄▄███▄▄· █████╗ ███╗   ███╗ ██████╗ ████████╗    ██████╗
 // ██║    ██║██║  ██║██╔═══██╗    ██║██╔════╝    ██╔════╝██╔══██╗████╗ ████║██╔═══██╗╚══██╔══╝    ╚════██╗
 // ██║ █╗ ██║███████║██║   ██║    ██║███████╗    ███████╗███████║██╔████╔██║██║   ██║   ██║         ▄███╔╝
@@ -20,29 +19,38 @@ import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 contract WhoIsSamot is ERC721Tradable {
     using SafeMath for uint256;
     address constant WALLET1 = 0xffe5CBCDdF2bd1b4Dc3c00455d4cdCcf20F77587;
-    uint256 constant public MAX_SUPPLY = 8888;
-    uint256 public maxToMint = 8;
-    uint256 public maxToMintWhitelist = 8;
+    address constant WALLET2 = 0xD9CC8af4E8ac5Cb5e7DdFffD138A58Bac49dAEd5;
+    uint256 public constant MAX_SUPPLY = 8888;
+    uint256 public maxToMint = 5;
+    uint256 public maxToMintWhitelist = 5;
     bool public saleIsActive = false;
     bool public preSaleIsActive = false;
     string _baseTokenURI;
     string _contractURI;
     address[] whitelistAddr;
 
-    constructor(address _proxyRegistryAddress, address[] memory addrs) ERC721Tradable("Who Is Samot?", "SAMOT", _proxyRegistryAddress) {
+    constructor(address _proxyRegistryAddress, address[] memory addrs)
+        ERC721Tradable("Who Is Samot?", "SAMOT", _proxyRegistryAddress)
+    {
         whitelistAddr = addrs;
-        for(uint i = 0; i < whitelistAddr.length; i++) {
+        for (uint256 i = 0; i < whitelistAddr.length; i++) {
             addAddressToWhitelist(whitelistAddr[i]);
         }
     }
 
     struct Whitelist {
         address addr;
-        uint hasMinted;
+        uint256 hasMinted;
     }
     mapping(address => Whitelist) public whitelist;
 
-    function baseTokenURI() public view virtual override returns (string memory) {
+    function baseTokenURI()
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         return _baseTokenURI;
     }
 
@@ -81,28 +89,40 @@ contract WhoIsSamot is ERC721Tradable {
         }
     }
 
-    function addAddressToWhitelist(address addr) onlyOwner public returns(bool success) {
+    function addAddressToWhitelist(address addr)
+        public
+        onlyOwner
+        returns (bool success)
+    {
         require(!isWhitelisted(addr), "Already whitelisted");
         whitelist[addr].addr = addr;
         whitelist[addr].hasMinted = 0;
         success = true;
     }
 
-    function addAddressesToWhitelist(address[] memory addrs) onlyOwner public returns(bool success) {
+    function addAddressesToWhitelist(address[] memory addrs)
+        public
+        onlyOwner
+        returns (bool success)
+    {
         whitelistAddr = addrs;
-        for(uint i = 0; i < whitelistAddr.length; i++) {
+        for (uint256 i = 0; i < whitelistAddr.length; i++) {
             addAddressToWhitelist(whitelistAddr[i]);
         }
         success = true;
     }
 
-    function isWhitelisted(address addr) public view returns (bool isWhiteListed) {
+    function isWhitelisted(address addr)
+        public
+        view
+        returns (bool isWhiteListed)
+    {
         return whitelist[addr].addr == addr;
     }
 
     function getNFTPrice() public view returns (uint256) {
         require(totalSupply() < MAX_SUPPLY, "Sale has already ended");
-        uint currentSupply = totalSupply();
+        uint256 currentSupply = totalSupply();
         if (currentSupply >= 8001) {
             return 12800000000000000000; // 8001 - 8888 12.8 ETH
         } else if (currentSupply >= 7001) {
@@ -124,22 +144,43 @@ contract WhoIsSamot is ERC721Tradable {
         }
     }
 
-    function mint(address to, uint numberOfTokens) public payable {
+    function mint(address to, uint256 numberOfTokens) public payable {
         require(saleIsActive, "Sale is not active.");
-        require(totalSupply().add(numberOfTokens) <= MAX_SUPPLY, "Sale has already ended.");
+        require(
+            totalSupply().add(numberOfTokens) <= MAX_SUPPLY,
+            "Sale has already ended."
+        );
         require(numberOfTokens > 0, "numberOfTokens cannot be 0");
-        require(getNFTPrice().mul(numberOfTokens) <= msg.value, "ETH sent is incorrect.");
-        if(preSaleIsActive) {
-            require(numberOfTokens <= maxToMintWhitelist, "Exceeds wallet pre-sale limit.");
+        require(
+            getNFTPrice().mul(numberOfTokens) <= msg.value,
+            "ETH sent is incorrect."
+        );
+        if (preSaleIsActive) {
+            require(
+                numberOfTokens <= maxToMintWhitelist,
+                "Exceeds wallet pre-sale limit."
+            );
             require(isWhitelisted(to), "Your address is not whitelisted.");
-            require(whitelist[to].hasMinted.add(numberOfTokens) <= maxToMintWhitelist, "Exceeds per wallet pre-sale limit.");
-            require(whitelist[to].hasMinted <= maxToMintWhitelist, "Exceeds per wallet pre-sale limit.");
-            whitelist[to].hasMinted = whitelist[to].hasMinted.add(numberOfTokens);
+            require(
+                whitelist[to].hasMinted.add(numberOfTokens) <=
+                    maxToMintWhitelist,
+                "Exceeds per wallet pre-sale limit."
+            );
+            require(
+                whitelist[to].hasMinted <= maxToMintWhitelist,
+                "Exceeds per wallet pre-sale limit."
+            );
+            whitelist[to].hasMinted = whitelist[to].hasMinted.add(
+                numberOfTokens
+            );
         } else {
-            require(numberOfTokens <= maxToMint, "Exceeds per transaction limit.");
+            require(
+                numberOfTokens <= maxToMint,
+                "Exceeds per transaction limit."
+            );
         }
 
-        for(uint i = 0; i < numberOfTokens; i++) {
+        for (uint256 i = 0; i < numberOfTokens; i++) {
             mintTo(to);
         }
     }
@@ -147,7 +188,11 @@ contract WhoIsSamot is ERC721Tradable {
     function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
         uint256 wallet1Balance = balance.mul(10).div(100);
+        uint256 wallet2Balance = balance.mul(85).div(100);
         payable(WALLET1).transfer(wallet1Balance);
-        payable(msg.sender).transfer(balance.sub(wallet1Balance));
+        payable(WALLET2).transfer(wallet2Balance);
+        payable(msg.sender).transfer(
+            balance.sub(wallet1Balance.add(wallet2Balance))
+        );
     }
 }
