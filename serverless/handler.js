2fff1b5f6eeffae740d6993d7f5245cf91be51e2
Leroy Ware <leroyware@Leroys-MacBook-Pro.local>
2021-10-23 17:52:00 -0400

    Updated Metadata API

--- serverless/handler.js
+++ serverless/handler.js
@@ -29,19 +29,27 @@ const CONTRACT_ABI = [,
 ];
 
 exports.token = async (event) => {
+  const assetsBucket = `samotclub-assets-${process.env.ENV}`
   const tokenId = event.pathParameters.tokenId
-  const body = await getMetadata(tokenId)
-  if (body && body.image && body.name && body.description && body.attributes) {
+  if (process.env.REVEAL) {
+    const body = await getMetadata(tokenId)
+    if (body && body.image && body.name && body.description && body.attributes) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(body)
+      }
+    } else {
+      let metadata = await s3.getObject(`metadata/default`, assetsBucket)
+      return {
+        statusCode: 200,
+        body: metadata.Body.toString()
+      }
+    }
+  } else {
+    let metadata = await s3.getObject(`metadata/default`, assetsBucket)
     return {
       statusCode: 200,
       body: JSON.stringify(body)
-    }
-  } else {
-    const assetsBucket = `samotclub-assets-${process.env.ENV}`
-    let metadata = await s3.getObject(`collection/contract`, assetsBucket)
-    return {
-      statusCode: 200,
-      body: metadata.Body.toString()
     }
   }
 }
