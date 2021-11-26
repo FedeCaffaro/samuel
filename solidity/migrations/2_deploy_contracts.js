79a7619ec60a120cbc18c7f1cb37c6472a418fdb
Nico <nico@MacBook-Pro-de-Nico-2.local>
2021-11-26 16:45:35 -0500

    ERC1155 model  enter the commit message for your changes. Lines starting

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
-const Pyramyd = artifacts.require("Pyramyd");
-const SamotToken = artifacts.require("SamotToken");
-const WhoIsSamot = artifacts.require("WhoIsSamot");
-const whitelist = require("./whitelist.json")
+// const Pyramyd = artifacts.require("Pyramyd");
+// const SamotToken = artifacts.require("SamotToken");
+// const WhoIsSamot = artifacts.require("WhoIsSamot");
+// const whitelist = require("./whitelist.json")
+const GoldenToken = artifacts.require("GoldenToken");
 
 module.exports = async function (deployer, network, address) {
     let proxyRegistryAddress = "";
@@ -10,7 +11,8 @@ module.exports = async function (deployer, network, address) {
     } else {
         proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
     }
-    await deployer.deploy(WhoIsSamot, proxyRegistryAddress, "Who Is Samot?", "SAMOTTEST", whitelist);
-    await deployer.deploy(Pyramyd, proxyRegistryAddress, "PYRAMYD", "PYRMDTEST");
-    await deployer.deploy(SamotToken, "Samot Token", "$AMOTTEST");
+    // await deployer.deploy(WhoIsSamot, proxyRegistryAddress, "Who Is Samot?", "SAMOTTEST", whitelist);
+    // await deployer.deploy(Pyramyd, proxyRegistryAddress, "PYRAMYD", "PYRMDTEST");
+    // await deployer.deploy(SamotToken, "Samot Token", "$AMOTTEST");
+    await deployer.deploy(GoldenToken, "Samot Golden Coin", "SGC","https://samotclub.mypinata.cloud/ipfs/QmTSjZpbh6cX9ZhWp3w21R6ZMdPeBzFda8CdhtrgRxPJtS",proxyRegistryAddress);
 };
