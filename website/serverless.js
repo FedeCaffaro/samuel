f1700e3db5afcd3496351b5b2b43aa154519b9f9
Leroy Ware <leroyware@Leroys-MBP.home>
2021-12-14 16:51:02 -0600

    Fix website deployment bucket

--- website/serverless.js
+++ website/serverless.js
@@ -2,14 +2,14 @@ const { Component } = require('@serverless/core')
 
 class Deploy extends Component {
   async default(inputs = { env: 'prod', name: 'samot-website' }) {
-    const template = await this.load('@serverless/template', "prod")
+    const template = await this.load('@serverless/template', inputs.env)
     const output = await template({
       template: {
         name: inputs.name,
         nextApp: {
           component: '@sls-next/serverless-component',
           inputs: {
-            bucketName: `samot-website2-prod`,
+            bucketName: `samot-website-${inputs.env}`,
           },
         },
       },
