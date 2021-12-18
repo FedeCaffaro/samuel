d4b391c70db5fa0fd4ae7f0f74ca4eefadf7e7b5
Federico Caffaro <fcaffaro@itba.edu.ar>
2021-12-17 22:12:30 -0300

    updates 1155

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -2,7 +2,8 @@
 // const WhoIsSamot = artifacts.require("WhoIsSamot");
 // const GoldenToken = artifacts.require("GoldenToken");
 // const SamotToken = artifacts.require("SamotToken");
-const SamotStaking = artifacts.require("SamotStaking");
+// const SamotStaking = artifacts.require("SamotStaking");
+const lindanene = artifacts.require("lindanene");
 
 module.exports = async function (deployer, network, address) {
     let proxyRegistryAddress = "";
@@ -15,5 +16,6 @@ module.exports = async function (deployer, network, address) {
     // await deployer.deploy(Pyramyd, proxyRegistryAddress, "PYRAMYD", "PYRMDTEST");
     // await deployer.deploy(SamotToken, "Samot Token", "$AMOT", "0x83C26562cE37959b870240B9c06b3e7fd72Edd8c");
     // await deployer.deploy(GoldenToken, "Samot Golden Coin", "SGC","https://samotclub.mypinata.cloud/ipfs/QmTSjZpbh6cX9ZhWp3w21R6ZMdPeBzFda8CdhtrgRxPJtS",proxyRegistryAddress);
-    await deployer.deploy(SamotStaking, "0x83C26562cE37959b870240B9c06b3e7fd72Edd8c", 161290322600000, "0x0EfE45Fbc07CD6fbc0c514A67Ec8ff0e03fD59B2");
+    // await deployer.deploy(SamotStaking, "0x83C26562cE37959b870240B9c06b3e7fd72Edd8c", 161290322600000, "0x0EfE45Fbc07CD6fbc0c514A67Ec8ff0e03fD59B2");
+    await deployer.deploy(lindanene, "Linda Nene", "LN", "https://samotclub.mypinata.cloud/ipfs/QmStiE4GcNEejF9BEWMZAgpbr3BoC2z4w8WBzNJyFpA8vN", proxyRegistryAddress);
 };
