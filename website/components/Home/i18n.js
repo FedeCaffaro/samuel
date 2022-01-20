330da3fa6c62383f146925f172fc1b21918858d1
Franco Gonzalez <fgonzalezorellano@est.frba.utn.edu.ar>
2022-01-20 00:51:06 -0300

    add translations

--- website/components/Home/i18n.js
+++ website/components/Home/i18n.js
@@ -1,5 +1,13 @@
 import i18n from 'i18next';
 
 i18n.addResources('en', 'Home', {
-  helloWorld: 'Hello World!',
+  helloWorld: 'Hello World!'
+});
+
+i18n.addResources('es', 'Home', {
+  helloWorld: 'Hola Mundo!'
+});
+
+i18n.addResources('pr', 'Home', {
+  helloWorld: 'Olá Mundo!'
 });
