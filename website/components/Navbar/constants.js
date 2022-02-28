ba10b30e69fb40360be96715d5bd09359e9398d0
Franco Gonzalez <fgonzalezorellano@est.frba.utn.edu.ar>
2022-02-28 15:51:12 -0300

    logo

--- website/components/Navbar/constants.js
+++ website/components/Navbar/constants.js
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 
 import { ROUTES } from '../../constants/routes';
 
-export const LOGO = '/assets/general/logo-samot.png';
+export const LOGO = '/assets/general/samot-white.png';
 export const DROPDOWN_ICON = '/assets/general/dropdown-icon.svg';
 export const MENU_ICON = '/assets/general/menu-icon.svg';
 
