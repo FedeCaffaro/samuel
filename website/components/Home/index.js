cb2dd46052ce7d66e33704efbdb849b9a5942d93
Franco Gonzalez <fgonzalezorellano@est.frba.utn.edu.ar>
2022-03-01 13:21:25 -0300

    move componets

--- website/components/Home/index.js
+++ website/components/Home/index.js
@@ -4,10 +4,9 @@ import i18next from 'i18next';
 import { useSelector } from 'react-redux';
 
 import { ROUTES } from '../../constants/routes';
-import { LOGO } from '../../constants/images-paths';
-import { MINT_FORM_WEB } from '../../constants/links';
 
 import styles from './styles.module.scss';
+import PublicHome from './components/PublicHome';
 
 function Home() {
   const wallet = useSelector((state) => state.settings.wallet);
@@ -23,20 +22,7 @@ function Home() {
               </Link>
             </>
           ) : (
-            <>
-              <img src={LOGO} className={styles.logo} />
-              <div className={styles['mint-button']}>
-                <a
-                  type="button"
-                  className={styles.mint}
-                  href={MINT_FORM_WEB}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {i18next.t('Home:mint')}
-                </a>
-              </div>
-            </>
+            <PublicHome />
           )}
         </div>
       </div>
