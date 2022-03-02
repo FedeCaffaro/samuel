82c41d9bcc2a7a9db6c4270107ed334b947a476c
Franco Gonzalez <fgonzalezorellano@est.frba.utn.edu.ar>
2022-03-02 01:31:12 -0300

    working loading

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UseWalletProvider } from 'use-wallet';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { useRouter } from 'next/router';
 import '../scss/application.scss';
 import '../config/i18n';
 
@@ -9,11 +10,31 @@ import store from '../redux/store';
 import Head from '../components/head';
 import styles from '../styles/styles.module.scss';
 import GeneralHooks from '../components/GeneralHooks';
+import LoadingWrapper from '../components/LoadingWrapper';
 
 // TODO: remove this line
 // import '../styles/styles-old.scss';
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleStart = () => setLoading(true);
+  const handleComplete = () => setLoading(false);
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
+
   return (
     <div className={styles.container}>
       <Provider store={store}>
@@ -26,9 +47,11 @@ function App({ Component, pageProps }) {
           <ToastContainer position="bottom-center" />
           <GeneralHooks />
           <Head />
-          <div className="main-wrapper">
-            <Component {...pageProps} />
-          </div>
+          <LoadingWrapper loading={loading} className={styles['loading-container']}>
+            <div className="main-wrapper">
+              <Component {...pageProps} />
+            </div>
+          </LoadingWrapper>
         </UseWalletProvider>
       </Provider>
     </div>
