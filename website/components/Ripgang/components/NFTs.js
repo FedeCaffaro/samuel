aca5ab95aafe9e92080bf97093ef9668d21f703b
IthilNah <alankasis@hotmail.com>
2022-05-03 19:22:25 -0300

    Added bolds

--- website/components/Ripgang/components/NFTs.js
+++ website/components/Ripgang/components/NFTs.js
@@ -17,16 +17,16 @@ function NFTs ()  {
           <br/>
           <div className="description-container">
             <p className="description">
-            Bohemian Groove Corp. y Samot Club presentan la RIPCOIN, pieza inaugural de la colección de la RIPGANG.
+            <span className="bold">Bohemian Groove Corp.</span> y <span className="bold">Samot Club</span> presentan la <span className="bold">RIPCOIN</span>, pieza inaugural de la <span className="bold">colección de la RIPGANG</span>.
             <br></br>
             <br></br>
-            Este proyecto busca expandir aún más el universo conocido (y por conocer) de cada uno de los artistas, a través de drops que no solo serán piezas coleccionables de edición limitada, sino también llaves para acceder a beneficios, contenidos y regalos exclusivos, porque seamos honestos ¿a quién no le gustan los regalos?
+            Este proyecto busca expandir aún más el universo conocido (y por conocer) de cada uno de los artistas, a través de drops que no solo serán <span className="bold">piezas coleccionables de edición limitada</span>, sino también llaves para acceder a <span className="bold">beneficios</span>, <span className="bold">contenidos</span> y <span className="bold">regalos exclusivos</span>, porque seamos honestos ¿a quién no le gustan los regalos?
             <br></br>
             <br></br>
-            Es por eso que con esta moneda como puntapié inicial - y en contra de toda lógica y la voluntad de nuestros contadores - les traemos 1312 ediciones free to claim* para que de acá a un año cuando haya gente pagando plata pelotuda por los jpgs de Bohemian Groove, no puedan decir que no se los avisamos.
+            Es por eso que con esta moneda como puntapié inicial - y en contra de toda lógica y la voluntad de nuestros contadores - les traemos <span className="bold">1312 ediciones free to claim*</span> para que de acá a un año cuando haya gente pagando plata pelotuda por los jpgs de Bohemian Groove, no puedan decir que no se los avisamos.
             <br></br>
             <br></br>
-            Para los que ya nos conocen, y a quienes acaban de llegar: No se duerman, y bienvenidos al universo de Bohemian Groove.
+            Para los que ya nos conocen, y a quienes acaban de llegar: No se duerman, y <span className="bold">bienvenidos al universo de Bohemian Groove</span>.
             <br></br>
             <br></br> 
             Atte.
